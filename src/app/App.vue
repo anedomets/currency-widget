4e131c6750d2033097beeb4b589503f5da276d40
Nedomets Alexandr <anedomets@yandex.ru>
2023-02-13 10:20:38 +0300

    Initial commit

---
+++ src/app/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div :class="'main'">
+<!--    <CurrencyCard v-if="this.show"/>-->
+<!--    <Currency v-if="!this.show"/>-->
+<!--    <button v-on:click="isShow">{{this.changeButtonText()}}</button>-->
+    <router-view />
+  </div>
+</template>
+
+<script src="./app.ts"></script>
+<style src="./app.css"></style>
---
+++ src/store/model.ts
@@ -0,0 +1,38 @@
+export interface State{
+    mask : MaskModel
+}
+
+export interface ModalWindow{
+    message: string | null,
+    show: boolean
+}
+
+export interface MaskModel {
+    modalWindow : ModalWindow | null
+    loadMask : LoadMask | null
+}
+
+export interface LoadMask{
+    show: boolean
+}
+
+export interface Currency {
+    id : bigint
+    currencyId : string
+    numCode : string
+    charCode : string
+    nominal : bigint
+    name : string
+    value : string
+    previous : string
+}
+
+export interface CurrencyRate {
+    id : bigint
+    date : string
+    previousDate : string
+    previousUrl : string
+    timestamp : string
+    createDate : string
+    currencies : Array<Currency>
+}
